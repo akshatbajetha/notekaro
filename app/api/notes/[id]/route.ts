223c51d53c82ba2af4971d4df70e9f515b69631c
Akshat Bajetha <akshatbajetha@gmail.com>
2025-05-08 21:31:43 +0530

    Bug fix

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -4,56 +4,23 @@ export async function GET(
   _req: Request,
   { params }: { params: { id: string } }
 ) {
-  try {
-    const note = await getNoteById(params.id);
+  const note = await getNoteById(params.id);
 
-    if (!note) {
-      return new Response(JSON.stringify({ error: "Note not found" }), {
-        status: 404,
-      });
-    }
-
-    return new Response(JSON.stringify(note));
-  } catch (error) {
-    return new Response(JSON.stringify({ error: "Internal server error" }), {
-      status: 500,
-    });
-  }
+  return new Response(JSON.stringify(note));
 }
 
 export async function PATCH(
   req: Request,
   { params }: { params: { id: string } }
 ) {
-  try {
-    const noteId = params.id;
-    const { title, content } = await req.json();
+  const noteId = params.id;
 
-    if (!title && !content) {
-      return new Response(
-        JSON.stringify({ error: "No data provided to update" }),
-        {
-          status: 400,
-        }
-      );
-    }
+  const { title, content } = await req.json();
 
-    const updatedNote = await updateNoteById({
-      noteId,
-      title,
-      content,
-    });
-
-    if (!updatedNote) {
-      return new Response(JSON.stringify({ error: "Note not found" }), {
-        status: 404,
-      });
-    }
-
-    return new Response(JSON.stringify(updatedNote));
-  } catch (error) {
-    return new Response(JSON.stringify({ error: "Internal server error" }), {
-      status: 500,
-    });
-  }
+  const updatedNote = await updateNoteById({
+    noteId,
+    title,
+    content,
+  });
+  return new Response(JSON.stringify(updatedNote));
 }
